17b5fb262162e26ed5c06c6ef279c8225a4cd28f
Josue_Can <canj2566@gmail.com>
2023-10-02 22:04:29 -0600

    correcciones de encarpetado

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,14 +1,75 @@
 import CardSaucer from "@/components/card-saucer/cardsaucer";
-import ContainerSauser from "./containersauser";
 import Search from "@/components/Search/search";
+import OptionsTabs from "@/components/Tabs/optionstabs";
+import { useState } from "react";
 
 export default function Index() {
+  const initialCardData = [
+    {
+      imageUrl:
+        "https://img.freepik.com/free-photo/pasta-spaghetti-with-shrimps-sauce_1220-5072.jpg?w=2000&t=st=1678041911~exp=1678042511~hmac=e4aa55e70f8c231d4d23832a611004f86eeb3b6ca067b3fa0c374ac78fe7aba6",
+      title: "Espaguetis con salsa de gambas ",
+      regularPrice: "$700.00",
+      salePrice: "$700.00",
+      description: "Descripcion de producto",
+      viewUrl: "foodiesapp://food/1002",
+    },
+    {
+      imageUrl:
+        "https://img.freepik.com/free-photo/pasta-spaghetti-with-shrimps-sauce_1220-5072.jpg?w=2000&t=st=1678041911~exp=1678042511~hmac=e4aa55e70f8c231d4d23832a611004f86eeb3b6ca067b3fa0c374ac78fe7aba6",
+      title: "Espaguetis con salsa de gambas ",
+      regularPrice: "$700.00",
+      salePrice: "$700.00",
+      description: "Descripcion de producto",
+      viewUrl: "foodiesapp://food/1002",
+    },
+    {
+      imageUrl:
+        "https://img.freepik.com/free-photo/pasta-spaghetti-with-shrimps-sauce_1220-5072.jpg?w=2000&t=st=1678041911~exp=1678042511~hmac=e4aa55e70f8c231d4d23832a611004f86eeb3b6ca067b3fa0c374ac78fe7aba6",
+      title: "Espaguetis con salsa de gambas ",
+      regularPrice: "$700.00",
+      salePrice: "$700.00",
+      description: "Descripcion de producto",
+      viewUrl: "foodiesapp://food/1002",
+    },
+    {
+      imageUrl:
+        "https://img.freepik.com/free-photo/pasta-spaghetti-with-shrimps-sauce_1220-5072.jpg?w=2000&t=st=1678041911~exp=1678042511~hmac=e4aa55e70f8c231d4d23832a611004f86eeb3b6ca067b3fa0c374ac78fe7aba6",
+      title: "Espaguetis con salsa de gambas ",
+      regularPrice: "$700.00",
+      salePrice: "$700.00",
+      description: "Descripcion de producto",
+      viewUrl: "foodiesapp://food/1002",
+    },
+    {
+      imageUrl:
+        "https://img.freepik.com/free-photo/pasta-spaghetti-with-shrimps-sauce_1220-5072.jpg?w=2000&t=st=1678041911~exp=1678042511~hmac=e4aa55e70f8c231d4d23832a611004f86eeb3b6ca067b3fa0c374ac78fe7aba6",
+      title: "Espaguetis con salsa de gambas ",
+      regularPrice: "$700.00",
+      salePrice: "$700.00",
+      description: "Descripcion de producto",
+      viewUrl: "foodiesapp://food/1002",
+    },
+  ];
+  const [cardData, setCardData] = useState(initialCardData);
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleTabChange = (index: number) => {
+    setActiveTab(index);
+    // Filtra los datos de la tarjeta según la pestaña activa
+    const filteredData = initialCardData.filter((_, i) => i === index);
+    setCardData(filteredData);
+  };
   return (
-    <>
-      <div className="">
+      <div className="container">
         <Search/>
-        <ContainerSauser/>
+        <OptionsTabs />
+      <h1 className="pt-5 text-xl pl-8">ELIGE LOS PLATILLOS:</h1>
+      <div className=" px-5 grid grid-flow-row gap-8 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 pt-8">
+        {cardData.map((card, index) => (
+          <CardSaucer key={index} {...card} />
+        ))}
       </div>
-    </>
+      </div>
   );
 }
--- src/components/Tabs/optionstabs.tsx
+++ src/components/Tabs/optionstabs.tsx
@@ -1,53 +1,36 @@
+import React, { useState } from 'react';
+import CardSaucer from "@/components/card-saucer/cardsaucer";
+
 export default function OptionsTabs() {
+  const [activeTab, setActiveTab] = useState<number>(0); 
+
+  const handleTabChange = (index: number) => { 
+    setActiveTab(index);
+  };
+
+  const handleEnterPress = () => {
+    console.log(`Mostrar contenido de la pestaña ${activeTab}`);
+  };
+
   return (
-    <>
-      <div className="space-y-4 px-10 pt-10 ">
-        <div className="border-b border-b-gray-700">
-          <ul className="-mb-px flex items-center gap-4 text-lg font-medium">
-            <li className="flex-1">
+    <div className="space-y-4 px-10 pt-10">
+      <div className="border-b border-b-gray-700">
+        <ul className="-mb-px flex items-center gap-4 text-lg font-medium">
+          {["Platos Principales", "Tacos", "Antojitos", "Postres", "Bebidas"].map((tab, index) => (
+            <li className="flex-1" key={index}>
               <a
                 href="#"
-                className="relative flex items-center justify-center gap-2 px-1 py-3 text-[#EA7C69] after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-full after:bg-[#EA7C69] hover:text-[#EA7C69]"
+                className={`relative flex items-center justify-center gap-2 px-1 py-3 ${activeTab === index ? 'text-[#EA7C69] after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-full after:bg-[#EA7C69]' : 'text-white hover:text-[#EA7C69]'}`}
+                onClick={() => handleTabChange(index)}
+                onKeyDown={(e) => e.key === 'Enter' && handleEnterPress()}
+                tabIndex={0} 
               >
-                Platos Principales
+                {tab}
               </a>
             </li>
-            <li className="flex-1">
-              <a
-                href="#"
-                className="flex items-center justify-center gap-2 px-1 py-3 text-white hover:text-[#EA7C69]"
-              > 
-              Tacos
-              </a>
-            </li>
-            
-            <li className="flex-1">
-              <a
-                href="#"
-                className="flex items-center justify-center gap-2 px-1 py-3 text-white hover:text-[#EA7C69]"
-              >
-                Antojitos
-              </a>
-            </li>
-            <li className="flex-1">
-              <a
-                href="#"
-                className="flex items-center justify-center gap-2 px-1 py-3 text-white hover:text-[#EA7C69]"
-              >
-                Postres
-              </a>
-            </li>
-            <li className="flex-1">
-              <a
-                href="#"
-                className="flex items-center justify-center gap-2 px-1 py-3 text-white hover:text-[#EA7C69]"
-              >
-                Bebidas
-              </a>
-            </li>
-          </ul>
-        </div>
+          ))}
+        </ul>
       </div>
-    </>
+    </div>
   );
 }
